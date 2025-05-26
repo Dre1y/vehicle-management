5547077c887a17c70945a0514a88bb6200f5763d
Andrey <andreymatheus92@gmail.com>
2025-05-26 18:44:37 -0300

    feat: add validation constraints for door quantity in CarRequestDTO and engine displacement in MotorcycleRequestDTO

--- api/src/main/java/com/company/api/DTOS/MotorcycleRequestDTO.java
+++ api/src/main/java/com/company/api/DTOS/MotorcycleRequestDTO.java
@@ -1,10 +1,14 @@
 package com.company.api.DTOS;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public class MotorcycleRequestDTO extends VehicleRequestDTO {
 
     @NotNull(message = "Cilindrada é obrigatória")
+    @Min(value = 50, message = "Cilindrada mínima é 50cc")
+    @Max(value = 2500, message = "Cilindrada máxima é 2500cc")
     private int engineDisplacement;
 
     public int getEngineDisplacement() {
